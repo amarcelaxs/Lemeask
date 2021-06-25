61903b4b8763eeac9d8d7e2ad88c7501de22a073
amarcelaxs <amarcelaxs@gmail.com>
2021-06-24 23:19:35 -0300

    finalizando o projeto

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-
+import cx from 'classnames';
 import './styles.scss'
 
 
@@ -11,16 +11,28 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
 
 export function Question({
   content,
   author,
+  isAnswered = false,
+  isHighlighted = false,
   children,
 }: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      //className={`question ${isAnswered ? 'answered' : ''} ${isHighlighted ? 'highlighted' : ''} `}
+      //ou usar plugin classnames para diminuir codigo de condições
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
